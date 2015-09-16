d0a7a1cbf7fa9e7ebef1e4ac63d1b4a277c31326
willgoishi <willgoishi@users.noreply.github.com>
2015-09-16 16:03:33 -0700

    Update employees.py
    Details added

--- src/employees.py
+++ src/employees.py
@@ -1,1 +1,8 @@
+#employee python file
+
+#Employee - 
+    #1:Input employee information and is able to store this information into database
+    #2:Employee information can be extracted 
+    #3:Employee's payroll and position standings are calculated and stored
+    #4:Employees can be sorted based on precedence or ordered based on information
 
