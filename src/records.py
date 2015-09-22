6c9c75428b83058513ee142ebe23f6582c852ff9
willgoishi <willgoishi@users.noreply.github.com>
2015-09-22 13:29:57 -0700

    Update records.py

--- src/records.py
+++ src/records.py
@@ -1,1 +1,46 @@
+# Records Python file
 
+#Records
+    #1:Records employees and equipment into databases
+    #2:Creates a record of sales for each year
+    #3:Creates a comparison model so that yearly income can be compared and contrasted
+    #4:Tracks the records of employee payroll 
+    
+    
+import sys;
+from tkinter import *#imports GUI library
+import sqlite3       #imports database library
+
+class records:#Records class that takes information and stores/removes information
+    
+    database = sqlite3.connect('records_information.db');#connects to database
+    c = database.cursor();
+    #c.execute('''CREATE TABLE employees(id INTEGER PRIMARY KEY, name TEXT,phone TEXT, job_type TEXT, email TEXT ) ''');
+    #database.commit();
+    c = database.cursor();                 #Drops table created
+    c.execute('''DROP TABLE employees'''); #     ........
+    database.commit();                     #     ........
+    database.close();#closes the database
+
+    #def _init_(self):
+        #
+    def store_employee(id_num, employee_name, phone, job_t, email):#stores employee into database
+        database = sqlite3.connect('records_information.db');
+        c = database.cursor();
+
+        c.execute('''INSERT INTO employees (id, name, phone, job, email) ''',(id_num, employee_name, phone, job_t, email));
+        print("Employee Recorded");
+        database.commit(); 
+        
+        database.close();
+        
+employees = [];#declares array of employees
+
+def retrieve_data(employees):#retrieves data<employee's information> from database then stores that information into an array
+    database = sqlite3.conect('records_information.db');
+    c.execute('''SELECT id, name, phone, job, email FROM employees''');
+    c = database.cursor();
+    employees = c.fetchall();
+    return employees;
+
+#def search_for_employee(employees):#
