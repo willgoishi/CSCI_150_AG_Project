ce70787c00b1905cbb426866986c8c78e11e6839
willgoishi <willgoishi@users.noreply.github.com>
2015-09-26 09:34:13 -0700

    Update to records.py
    added additional function to access databases for sales info

--- src/records.py
+++ src/records.py
@@ -7,7 +7,7 @@
     #4:Tracks the records of employee payroll 
     
     
-import sys;
+import sys
 from tkinter import *#imports GUI library
 import sqlite3       #imports database library
 
@@ -16,24 +16,31 @@ class records:#Records class that takes information and stores/removes informati
     def _init_(self, daily, monthly, yearly):
         #
 
-    def create_database_entry():
+    def create_database_employees():
         database = sqlite3.connect('records_information.db');#connects to database
         c = database.cursor();
-        c.execute('''CREATE TABLE employees(id INTEGER PRIMARY KEY, name TEXT,phone TEXT, job_type TEXT, email TEXT ) ''');
+        c.execute('''CREATE TABLE employees(id INTEGER PRIMARY KEY, name TEXT,phone TEXT, job_type TEXT, email TEXT, year_employeed TEXT, Salary_paid TEXT ) ''');
         database.commit();
         database.close();
         
-    def drop_data_base():
-        c = database.cursor();                 #Drops table created
-        c.execute('''DROP TABLE employees'''); #     ........
-        database.commit();                     #     ........
+    def create_database_sales():
+        database = sqlite3.connect('sales_records.db'); #connects to database that has information on sales
+        c = database.cursor();
+        c.execute('''CREATE TABLE sales()''');#inputs sales information
+        database.commit();
+        database.close();
+        
+    def drop_data_base(table_name):
+        c = database.cursor();                       #Drops table created
+        c.execute('''DROP TABLE %s''',(table_name)); #     ........
+        database.commit();                           #     ........
         database.close();#closes the database
 
-    def store_employee(id_num, employee_name, phone, job_t, email):#stores employee into database
+    def store_employee(id_num, employee_name, phone, job_t, email):#stores employee into records database
         database = sqlite3.connect('records_information.db');
         c = database.cursor();
 
-        c.execute('''INSERT INTO employees (id, name, phone, job, email) ''',(id_num, employee_name, phone, job_t, email));
+        c.execute('''INSERT INTO employees (id, name, phone, job, email, year, salary) ''',(id_num, employee_name, phone, job_t, email,year_employeed TEXT, Salary_paid TEXT));
         print("Employee Recorded");
         database.commit(); 
         
@@ -43,7 +50,7 @@ employees = [];#declares array of employees
 
 def retrieve_data(employees):#retrieves data<employee's information> from database then stores that information into an array
     database = sqlite3.conect('records_information.db');
-    c.execute('''SELECT id, name, phone, job, email FROM employees''');
+    c.execute('''SELECT id, name, phone, job, email, year, salary FROM employees''');
     c = database.cursor();
     employees = c.fetchall();
     return employees;
