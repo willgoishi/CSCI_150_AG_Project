89103cca268efbce0d09da832eddbc466539cb94
andysokk <andysokk@mail.fresnostate.edu>
2015-09-30 06:05:25 +0800

    Update income.py

--- src/income.py
+++ src/income.py
@@ -1,1 +1,23 @@
+class Employees:
+    def __init__(self, month):
+        self.month = month
+        self.item = item
+        self.amount = amount
+        self.price = price
+        self.income = income        
+
+    def printMonth(self):
+        print ("Month is %s.") % (self.month)
+
+    def printItem(self):
+        print ("item is %s.") % (self.item)
+        
+    def printAmount(self):
+        print ("Quantity of %s sold during %s is %s") % (self.item, self.month, self.amount)
+        
+    def printPrice(self):
+        print ("Price of %s is %s ") % (self.item, self.price)
+
+    def printIncome(self):
+        print ("%s\'s is %s") % (self.month, self.income)
 
