1dc3a99929b3a0ac3bb3e77186f70ce0676a9c8f
aienkoji <aienko@mail.fresnostate.edu>
2015-10-22 18:02:11 -0700

    Update clients.py

--- src/clients.py
+++ src/clients.py
@@ -19,8 +19,7 @@ class clients:
         c = client_database.cursor();
         
         #creating table
-        c.execute('''CREATE TABLE customers (ID INT, name TEXT, company TEXT, phone TEXT, email TEXT, product TEXT, quantity TEXT)''')
-
+        c.execute('''CREATE TABLE customers (ID INT, name TEXT, company TEXT, phone TEXT, email TEXT)''')
         client_database.commit();
         client_database.close();
 
@@ -32,12 +31,22 @@ class clients:
         client_database.commit();
         client_database.close();
     
+    def delete_table(choice):
+        client_database=sqlite3.connect('client_data.db');
+        c=client_database.cursor();
+        c.execute('''DROP TABLE ''' + choice + ''' ''');
+        client_database.commit();
+        client_database.close();
+    
     #stores client info in database (add client)
     def store_client_info(ID, name, company, phone, email):
         client_database = sqlite3.connect('client_data.db');
         c=client_database.cursor();
         c.execute('''INSERT INTO customers (ID ,name, company, phone, email) VALUES(?,?,?,?,?)''',(ID, name, company, phone, email));
-        print("Client info entered");
+        print("Client info entered:", name, company);
+        title=name+company;
+        c.execute('''CREATE TABLE '''+ title +'''(product TEXT, quantity INT)''')
+        print("purchase table created:", title);
         client_database.commit();
         client_database.close();
 
@@ -48,13 +57,14 @@ class clients:
         c=client_database.cursor();
         c.execute('''SELECT ID, name, company, phone, email FROM customers''');
         customers=c.fetchall();
+        for row in customers: print(row);
         return customers;
             
         
-    def store_sales_info (ID, name, company, product, quantity):
+    def store_sales_info (product, quantity):
         client_database = sqlite3.connect('client_data.db');
         c=client_database.cursor();
-        c.execute('''INSERT INTO customers(product, quantity)''', (product, quantity));
+        c.execute('''INSERT INTO purchases(product, quantity) VALUES(?,?)''', (product, quantity));
         print("purchase info entered");
         client_database.commit();
         client_database.close();
@@ -63,7 +73,7 @@ class clients:
 
     def retrieve_sales_info (sales):
         client_database=sqlite3.connect('client_data.db');
-        c.execute('''Select ID, name, company, phone, email, product, quantity FROM customers''');
+        c.execute('''SELECT ID, name, company, phone, email, product, quantity FROM customers''');
         c=client_database.cursor();
         sales=c.fetchall();
         return sales;
