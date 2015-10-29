f50bee36edcd039ca5348371daa9cc85e5250c2f
aienkoji <aienko@mail.fresnostate.edu>
2015-10-29 15:09:24 -0700

    Update clients.py

--- src/clients.py
+++ src/clients.py
@@ -19,7 +19,7 @@ class clients:
         c = client_database.cursor();
         
         #creating table
-        c.execute('''CREATE TABLE customers (ID INT, name TEXT, company TEXT, phone TEXT, email TEXT)''')
+        c.execute('''CREATE TABLE customers (ID INT, name TEXT, phone TEXT, email TEXT)''')
         client_database.commit();
         client_database.close();
 
@@ -39,14 +39,14 @@ class clients:
         client_database.close();
     
     #stores client info in database (add client)
-    def store_client_info(ID, name, company, phone, email):
+    #stores client info in database (add client)
+    def store_client_info(ID, name, phone, email):
         client_database = sqlite3.connect('client_data.db');
         c=client_database.cursor();
-        c.execute('''INSERT INTO customers (ID ,name, company, phone, email) VALUES(?,?,?,?,?)''',(ID, name, company, phone, email));
-        print("Client info entered:", name, company);
-        title=name+company;
-        c.execute('''CREATE TABLE '''+ title +'''(product TEXT, quantity INT)''')
-        print("purchase table created:", title);
+        c.execute('''INSERT INTO customers (ID ,name, phone, email) VALUES(?,?,?,?)''',(ID, name, phone, email));
+        print("CLIENT INFO ENTERED:", name);
+        c.execute('''CREATE TABLE '''+ name +'''(product TEXT, quantity INT)''')
+        print("PURCHASE TABLE CREATED:", name);
         client_database.commit();
         client_database.close();
 
@@ -55,31 +55,68 @@ class clients:
     def retrieve_client_data():
         client_database = sqlite3.connect('client_data.db');
         c=client_database.cursor();
-        c.execute('''SELECT ID, name, company, phone, email FROM customers''');
+        c.execute('''SELECT ID, name, phone, email FROM customers''');
         customers=c.fetchall();
+        print("RETRIEVING CLIENT DATA:");
         for row in customers: print(row);
         return customers;
             
         
-    def store_sales_info (product, quantity):
+          
+    def store_sales_info (name, product, quantity):
         client_database = sqlite3.connect('client_data.db');
         c=client_database.cursor();
-        c.execute('''INSERT INTO purchases(product, quantity) VALUES(?,?)''', (product, quantity));
-        print("purchase info entered");
+        c.execute('''INSERT INTO ''' + name+ '''(product, quantity) VALUES(?,?)''',(product, quantity));
+        print("PURCHASE INFO ENTERED:", name);
         client_database.commit();
         client_database.close();
         
         sales = [];
-
-    def retrieve_sales_info (sales):
+        
+    def retrieve_sales_info (choice):
         client_database=sqlite3.connect('client_data.db');
-        c.execute('''SELECT ID, name, company, phone, email, product, quantity FROM customers''');
         c=client_database.cursor();
+        c.execute('''SELECT product, quantity FROM ''' + choice +''' ''');
         sales=c.fetchall();
+        print("RETRIEVING SALES INFO:", choice);
+        for row in sales: print(row);
         return sales;
 
-    def search (name, company):
-        
+
+    def retrieve_one_client(n):
+        print("RETRIEVING ONE CLIENT:", n);
+        client_database=sqlite3.connect('client_data.db');
+        c=client_database.cursor();
+        c.execute('''SELECT ID, name, phone, email FROM customers WHERE name=(?) ''', [n]);
+        searchvar=c.fetchall();
+        for row in searchvar:
+            print(row);
+    
+    def search(searchname):
+        namearray=[];
+        client_database=sqlite3.connect('client_data.db');
+        c=client_database.cursor();
+        c.execute('''SELECT ID, name, phone, email FROM customers''');
+        searchvar=c.fetchall();
+        for row in searchvar:
+            namearray.append(row[1]);
+        i=0;
+        length=len(namearray);
+        while(i<length):
+            if(namearray[i]==searchname):
+                print("SEARCH FOUND:", namearray[i]);
+                c.execute('''SELECT ID, name, phone, email FROM customers WHERE name=(?) ''', [namearray[i]]);
+                searchvar=c.fetchall();
+                for row in searchvar:
+                    print(row);
+                c.execute('''SELECT product, quantity FROM ''' + namearray[i] +''' ''');
+                sales=c.fetchall();
+                for row in sales:
+                    print(row);
+                i=i+1;
+            else: i=i+1;
+
+
 
         
         
