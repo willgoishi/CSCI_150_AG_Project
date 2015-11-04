82368768792cc977305abf8e6e0f484e70a2a1f6
willgoishi <willgoishi@users.noreply.github.com>
2015-11-03 20:43:09 -0800

    Update records.py
    Added new database storage and retrieval for employees payroll.
    -Create database
    -destory database
    -store payroll
    -given a specific date, return the payroll sum for that year

--- src/records.py
+++ src/records.py
@@ -132,4 +132,64 @@ class records:#Records class that takes information and stores/removes informati
         database.commit(); #commits all actions related to the database
 
         database.close();    #closes database
+        
+    def create_database_payroll():                                                  #creates database payroll that will store payroll for employees
+        database = sqlite3.connect('payroll.db');
+        c = database.cursor();
+        c.execute('''CREATE TABLE payroll (name TEXT, year TEXT, salary INTEGER) ''');
+        database.commit();
+        database.close();
 
+    def store_employee_with_salary(name_employee, year_paid, salary_amount):        #stores a single set of data relating to one individual's salary for a year
+        database = sqlite3.connect('payroll.db');
+        cursor = database.cursor();
+        cursor.execute('''INSERT INTO payroll(name, year, salary) VALUES (?,?,?) ''',(name_employee, year_paid, salary_amount));
+        database.commit();
+        database.close();
+
+    def delete_payroll_database():
+        database = sqlite3.connect('payroll.db');
+        c = database.cursor();
+        c.execute('''DROP TABLE payroll ''');
+        database.commit();
+        database.close();
+        
+    def print_payroll_from_designated_year(year):                                   #Prints the salary for a year based on what year user inputs 
+        database = sqlite3.connect('payroll.db');
+        controller = database.cursor();
+
+        year_salary_paid = []; #array to store the years at which salaries were paid
+        salary = [];           #array to store the salary values that were actually paid
+        payroll_year = [];     #array that will be used to store years so that it will revoke repetitive printing of yearly payrolls
+        
+        database_payroll = database.execute('SELECT year, salary FROM payroll');  #executes a database command that will retrieve the data by row from database
+        for row in database_payroll:            #this segment takes the information from the database and stores them seperately into individual arrays
+            year_salary_paid.append(row[0]);
+            salary.append(row[1]);
+            
+        #section that sorts years and adds similar salaries for a year and prints out the salaries for the desired year    
+        a = 0;   #first tracer for traversal through array
+        b = a+1; #second tracer for traversal through array
+        
+        while a < len(year_salary_paid):
+            salary_prime = salary[a]; #sets the value of salary_prime
+            while a < len(year_salary_paid) and b < len(year_salary_paid):     #as long as the length of a and b are less than size of salary year_salary array...
+                if(year_salary_paid[a] == year_salary_paid[b]):                #then can allow the following to take place, meaning it checks if array is empty or not
+                    salary_prime = salary_prime + salary[b];     #if the years are matching for different segments of the array, then it adds the salaries towards a total sum
+                    b += 1;                                 # increments b to allow traversal through the array
+                else:
+                    b += 1;                                 # increments b to allow traversal through the array
+                
+            if not (year_salary_paid[a] in payroll_year): #condition set to check if a year has been seen and printed already
+                if year == year_salary_paid[a]:
+                    print("Year:", year_salary_paid[a], "Payroll Total For Year:", salary_prime); #prints the year and the total payroll for that year
+                elif year != year_salary_paid[a]:
+                    pass;
+                else:    #checks and print not recorded if the data doesnt exist in memory
+                    print("Error: Date Not Recorded");
+                payroll_year.append(year_salary_paid[a]); #appends the year into the payroll_year so that future printing of the same data does not occur
+
+            a += 1; #increments the first position to the next so another traversal can take place 
+            b = a + 1;#resets the value of <b>
+        database.commit();
+        database.close();
