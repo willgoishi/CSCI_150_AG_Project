26621eb719e2a221b238b404db9b368cc86d002b
Jschott9 <schotty132@yahoo.com>
2015-12-15 20:54:25 -0800

    Update EmployeeDB.py

--- src/Employee-s/EmployeeDB.py
+++ src/Employee-s/EmployeeDB.py
@@ -1,13 +1,13 @@
 import sqlite3
 
 def DatabaseConnection():
-    return sqlite3.connect('EMPLOYEE\'S.db')
+    return sqlite3.connect('EMPLOYEES.db')
 
 class EmployeesDatabase():
     def createTable(self):
-        EmployeeDB = DatabaseConnection()  
+        EmployeeDB = DatabaseConnection()   # the database is assign to the variable InventoryDB
         cursor = EmployeeDB.cursor()
-        cursor.execute('''CREATE TABLE Employee\'s
+        cursor.execute('''CREATE TABLE Employees
                       (Name TEXT, Number TEXT, Email TEXT unique, Position TEXT, Salary TEXT)''')
         EmployeeDB.commit()
         EmployeeDB.close()
@@ -16,18 +16,23 @@ class EmployeesDatabase():
         EmployeeDB = DatabaseConnection()
         cursor = EmployeeDB.cursor()
         cursor.execute('''
-            INSERT INTO Employee\'s ( Name, Number, Email, Position, Salary) VALUES (?, ?, ?, ?, ?)''',
+            INSERT INTO Employees ( Name, Number, Email, Position, Salary) VALUES (?, ?, ?, ?, ?)''',
             (data.name, data.number, data.email, data.position, data.salary))
         EmployeeDB.commit()
         EmployeeDB.close()
 
-    employeeInfo = []
 
-    def retrieve_data(employeeInfo):
-        EmployeeDB = DatabaseConnection()
+    def retrieve_data(self):
+        employeeInfo = []
+        EmployeeDB = DatabaseConnection();
         cursor = EmployeeDB.cursor();
-        cursor.execute('''SELECT Name, Number, Email, Position, Salary FROM Employee\'s''');
+        cursor.execute('''SELECT Name, Number, Email, Position, Salary FROM Employees''');
         employeeInfo = cursor.fetchall();
         return employeeInfo;
 
-
+    def delete_table(self):
+        EmployeeDB = DatabaseConnection();
+        cursor = EmployeeDB.cursor();
+        cursor.execute('''DROP TABLE Employees''')
+        EmployeeDB.commit()
+        EmployeeDB.close()
